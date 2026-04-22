5803844e6d18520e120c8f454a4cc0615a030fa0
Aka21 <asho@aka21.com>
2026-04-22 16:37:11 +0800

    chore(git): remove orphaned test scripts and optimize gitignore rules

--- test_rar_pwd.rs
+++
@@ -1,8 +0,0 @@
-use std::fs::File;
-
-fn main() {
-    let pwd = "test";
-    let file = File::open("tests/payloads/encrypted_test.rar").unwrap();
-    let mut reader = rar::RarReader::new_from_file(file);
-    // well rar crate API isn't exactly public for internal parsing, let's just use it
-}
