16260f436b04ac377a947defbc926ac92d44d327
Aka21 <asho@aka21.com>
2026-04-30 15:15:29 +0800

    chore: project housekeeping and cleanup of unused test files

--- wasm-sandbox/src/test_crc.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let archive =
-        rar::Archive::extract_all("tests/payloads/encrypted_test.rar", "/tmp/", "test").unwrap();
-    let f = &archive.files[0];
-    println!("crc: {}", f.data_crc);
-}
--- wasm-sandbox/src/test_zip_error.rs
+++
@@ -1,9 +0,0 @@
-use zip::result::ZipError;
-
-fn main() {
-    let err = ZipError::UnsupportedArchive("Password required to decrypt file");
-    match err {
-        ZipError::UnsupportedArchive(msg) if msg.contains("Password required") => println!("Matched msg: {}", msg),
-        e => println!("Other: {}", e),
-    }
-}
